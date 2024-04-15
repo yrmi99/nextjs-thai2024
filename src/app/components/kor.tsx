d9b2407fb2ab31e318ccb46c2e4cc446c6c0b0c7
yseo60 <yseo60@gatech.edu>
2024-04-14 21:40:33 -0400

    update fundraising progress

--- src/app/components/kor.tsx
+++ src/app/components/kor.tsx
@@ -152,7 +152,7 @@ export default function Eng() {
         </div>
         <div className="flex justify-center">
           <span className="font-medium text-sm sm:text-base text-center text-[#e88247]">
-            진행 상황: 14.1% ($691.64)
+            진행 상황: 16.2% ($791.64)
           </span>
         </div>
         <div className="flex justify-center">
