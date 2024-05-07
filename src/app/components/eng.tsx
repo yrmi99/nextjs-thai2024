b2ea870930f216adfa9e5616960326f2afb6951a
yseo60 <yseo60@gatech.edu>
2024-05-07 13:28:51 +0900

    update progress

--- src/app/components/eng.tsx
+++ src/app/components/eng.tsx
@@ -162,7 +162,7 @@ export default function Eng() {
         </div>
         <div className="flex justify-center">
           <span className="font-medium text-sm sm:text-base text-center text-[#e88247]">
-            Progress: 48.17% ($2360.49)
+            Progress: 52.25% ($2560.49)
           </span>
         </div>
         <div className="flex justify-center">
--- src/app/components/kor.tsx
+++ src/app/components/kor.tsx
@@ -155,7 +155,7 @@ export default function Eng() {
         </div>
         <div className="flex justify-center">
           <span className="font-medium text-sm sm:text-base text-center text-[#e88247]">
-            진행 상황: 48.17% ($2360.49)
+            진행 상황: 52.25% ($2560.49)
           </span>
         </div>
         <div className="flex justify-center">
