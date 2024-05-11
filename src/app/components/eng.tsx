bc987cc2041d00adc4e08fad8ed57bb1fda91a90
yseo60 <yseo60@gatech.edu>
2024-05-11 09:01:06 +0900

    update progress

--- src/app/components/eng.tsx
+++ src/app/components/eng.tsx
@@ -162,7 +162,7 @@ export default function Eng() {
         </div>
         <div className="flex justify-center">
           <span className="font-medium text-sm sm:text-base text-center text-[#e88247]">
-            Progress: 52.25% ($2560.49)
+            Progress: 47.61% ($2332.98)
           </span>
         </div>
         <div className="flex justify-center">
